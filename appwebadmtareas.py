c6b6aa470a83cb0e539a6d8ef4d1f6673b6ecb7a
Lorenzo <129416100+LoloGre@users.noreply.github.com>
2023-09-08 19:54:20 -0300

    Add files via upload

---
+++ appwebadmtareas.py
@@ -0,0 +1,81 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.secret_key = 'your_secret_key'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    tasks = db.relationship('Task', backref='user', lazy=True)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+@app.route('/')
+def index():
+    if 'user_id' in session:
+        user = User.query.filter_by(id=session['user_id']).first()
+        tasks = Task.query.filter_by(user=user, complete=False).all()
+        return render_template('index.html', tasks=tasks)
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('¡Registro exitoso! Inicia sesión para continuar.')
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
+        flash('Credenciales incorrectas. Inténtalo de nuevo.')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
+
+@app.route('/add', methods=['POST'])
+def add():
+    if 'user_id' in session:
+        user = User.query.filter_by(id=session['user_id']).first()
+        task_content = request.form['content']
+        new_task = Task(content=task_content, user=user)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/complete/<int:task_id>')
+def complete(task_id):
+    if 'user_id' in session:
+        task = Task.query.get(task_id)
+        task.complete = True
+        db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+
